55c2712299d16ca7d53592f0982960d4621b675d
Simon Johansson <simon.johansson@satcube.com>
2024-01-22 12:35:22 +0100

    it works with print!

--- md407/memory.x
+++ md407/memory.x
@@ -1,12 +1,45 @@
-/*
-	Default linker script for MD407 (STM32F407)
-	All code and data goes to RAM.
-*/
-/* Memory Spaces Definitions */
+/* MEMORY
+{ */
+  /* NOTE 1 K = 1 KiBi = 1024 bytes */
+  /* TODO Adjust these memory regions to match your device memory layout */
+  /* These values correspond to the LM3S6965, one of the few devices QEMU can emulate */
+  /* FLASH : ORIGIN = 0x00000000, LENGTH = 256K
+  RAM : ORIGIN = 0x20000000, LENGTH = 108K
+} */
+
 MEMORY
 {
-  FLASH (rx) : ORIGIN = 0x08000000, LENGTH = 1024K
-	RAM (xrw) : ORIGIN = 0x20000000, LENGTH = 112K
+  /* NOTE 1 K = 1 KiBi = 1024 bytes */
+  /* TODO Adjust these memory regions to match your device memory layout */
+  /* These values correspond to the LM3S6965, one of the few devices QEMU can emulate */
+  /* FLASH : ORIGIN = 0x20000000, LENGTH = 60K
+  RAM : ORIGIN = 0x2000F000, LENGTH = 52K */
+  RAM : ORIGIN = 0x20000000, LENGTH = 112K
+  VECTOR: ORIGIN = 0x2001C000, LENGTH = 2048
 }
 
-_stack_start = ORIGIN(RAM) + LENGTH(RAM);
+/* This is where the call stack will be allocated. */
+/* The stack is of the full descending type. */
+/* You may want to use this variable to locate the call stack and static
+   variables in different memory regions. Below is shown the default value */
+/* _stack_start = ORIGIN(RAM) + LENGTH(RAM); */
+
+/* You can use this symbol to customize the location of the .text section */
+/* If omitted the .text section will be placed right after the .vector_table
+   section */
+/* This is required only on microcontrollers that store some configuration right
+   after the vector table */
+/* _stext = ORIGIN(FLASH) + 0x400; */
+
+/* Example of putting non-initialized variables into custom RAM locations. */
+/* This assumes you have defined a region RAM2 above, and in the Rust
+   sources added the attribute `#[link_section = ".ram2bss"]` to the data
+   you want to place there. */
+/* Note that the section will not be zero-initialized by the runtime! */
+/* SECTIONS {
+     .ram2bss (NOLOAD) : ALIGN(4) {
+       *(.ram2bss);
+       . = ALIGN(4);
+     } > RAM2
+   } INSERT AFTER .bss;
+*/
--- md407/src/main.rs
+++ md407/src/main.rs
@@ -2,81 +2,62 @@
 #![no_std]
 #![feature(type_alias_impl_trait)]
 
+
+use cortex_m::peripheral::SCB;
+use cortex_m_semihosting::hprintln;
+use hal::{uart::{Config, Serial, Tx}, pac::USART1};
 use panic_halt as _;
-use defmt_brtt as _; // global logger
+//
+//
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal as hal;
+use cortex_m_rt::{entry, pre_init};
+use crate::hal::{pac, prelude::*};
+use core::fmt::Write;
+use cortex_m_semihosting::syscall;
 
-#[rtic::app(
-    device = stm32f4xx_hal::pac,
-    dispatchers = [USART1],
-    peripherals = true
+#[pre_init]
+unsafe fn startup() {
+    (*SCB::PTR).ccr.modify(|r| r & !(1 << 3));
+}
 
-)]
-mod app {
-    use stm32f4xx_hal::pac::{Interrupt, EXTI};
-    use stm32f4xx_hal::prelude::*; 
+#[entry]
+fn main() -> ! {
+    if let Some(dp) = 
+        pac::Peripherals::take()
+     {
+         let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(168.MHz()).pclk1(8.MHz()).freeze();
+        let gpiob = dp.GPIOB.split();
+        let mut led = gpiob.pb1.into_push_pull_output();
+        // Create a delay abstraction based on SysTick
+        let mut delay = dp.TIM5.delay_us(&clocks);
 
-    // Shared resources go here
-    #[shared]
-    struct Shared {
-        // TODO: Add resources
-    }
-
-    // Local resources go here
-    #[local]
-    struct Local {
-        button: EXTI,
-    }
-
-    #[init]
-    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
-        let mut p = ctx.device;
-        let cp = ctx.core;
-        let mut rcc = p.RCC.constrain();
         
-        let gpioa = p.GPIOA.split();
-        let syscfg = p.SYSCFG;
-        let exti = p.EXTI;
-        let button = gpioa.pa0;
-        
-        
-
-        cortex_m::interrupt::free(|cs| {
-            button.into_pull_down_input();
-            syscfg.exticr1.modify(|_, w| unsafe { w.exti0().bits(1) });
-
-            // Set interrupt request mask for line 1
-            exti.imr.modify(|_, w| w.mr0().set_bit());
-
-            // Set interrupt rising trigger for line 1
-            exti.rtsr.modify(|_, w| w.tr0().set_bit());
-            let mut nvic = cp.NVIC;
-            unsafe {
-                nvic.set_priority(Interrupt::EXTI0, 1);
-                cortex_m::peripheral::NVIC::unmask(Interrupt::EXTI0);
-            }
-            cortex_m::peripheral::NVIC::unpend(Interrupt::EXTI0);
-        });
-
-        (
-            Shared {
-                // Initialization of shared resources go here
-            },
-            Local {
-                button: exti,
-
-            },
-            init::Monotonics()
-
+        let tx_pin = gpiob.pb6.into_alternate();
+        let mut tx = dp
+        .USART1
+        .tx(
+            tx_pin,
+            Config::default()
+                .baudrate(115200.bps())
+                .wordlength_8()
+                .parity_none(),
+            &clocks,
         )
-    }
+        .unwrap();
+        
+        
+        writeln!(tx, "wooooow\r").unwrap();
 
-    // TODO: Add tasks
-
-    #[task(binds = EXTI0, local = [button])]
-    fn toggle_task(ctx: toggle_task::Context) {
-        defmt::info!("button pressed");
-        cortex_m::interrupt::free(|_cs| { 
-           ctx.local.button.pr.write(|w| w.pr0().set_bit()); 
-        });
+        loop {
+            led.set_high();
+            delay.delay_ms(300);
+            led.set_low();
+            delay.delay_ms(300);
+        }
     }
+    loop {
+    } 
+
 }
