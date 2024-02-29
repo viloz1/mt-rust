4e891045d762ad7f2e4349ed9ac23aea8370ea8d
Simon Johansson <simon.johansson@satcube.com>
2024-02-29 10:21:53 +0100

    reaction time is working on pico (isch, probe-rs is weird...)

--- pico/src/bin/reaction.rs
+++ pico/src/bin/reaction.rs
@@ -19,26 +19,26 @@ mod app {
     use rp_pico::XOSC_CRYSTAL_FREQ;
     use embedded_hal::digital::v2::OutputPin;
     use rp2040_hal::gpio::Interrupt::LevelHigh;
-    use test_app::{get_random_u16, get_random_byte};
-
-    pub struct Time {
-        seconds: u32,
-        milliseconds: u16
-    }
+    use test_app::{get_random_u16, get_random_byte, TimerRegs, PointerWrapper, time_us_64, CPU_PERIOD};
 
     // Shared resources go here
     #[shared]
     struct Shared {
         in_pin: Pin<Gpio26, FunctionSio<SioOutput>, rp2040_hal::gpio::PullDown>,
         rosc: RingOscillator<Enabled>,
-        timer: Time
+        timer_regs: TimerRegs,
+        sleep_time: u64,
+        interrupt_start_time: u64
     }
 
     // Local resources go here
     #[local]
     struct Local {
+        number_of_tasks: u16,
+        last_timer_value: u64,
         led_pin: Pin<Gpio25, FunctionSio<SioOutput>, rp2040_hal::gpio::PullDown>,
         led_state: bool
+
     }
 
     #[monotonic(binds = TIMER_IRQ_0, default = true)]
@@ -68,8 +68,7 @@ mod app {
 
         let rosc = rp2040_hal::rosc::RingOscillator::new(pac.ROSC);
         
-
-        // The single-cycle I/O block controls our GPIO pins
+               // The single-cycle I/O block controls our GPIO pins
         let sio = rp_pico::hal::Sio::new(pac.SIO);
 
         // Set the pins to their default state
@@ -79,11 +78,12 @@ mod app {
             sio.gpio_bank0,
             &mut pac.RESETS,
         );
+        let mut led_pin = pins.gpio25.into_push_pull_output();
+        led_pin.set_low().unwrap();
 
         // Configure GPIO 25 as an output to drive our LED.
         // we can use reconfigure() instead of into_pull_up_input()
         // since the variable we're pushing it into has that type
-        let led = pins.gpio25.reconfigure();
 
         // Set up the GPIO pin that will be our input
         let in_pin = pins.gpio26.into_push_pull_output();
@@ -102,24 +102,28 @@ mod app {
         
         }
 
-        let mono = Rp2040Monotonic::new(pac.TIMER);
         
+        let timer_regs = TimerRegs {
+            hi: PointerWrapper(pac.TIMER.timerawh.as_ptr()),
+            lo: PointerWrapper(pac.TIMER.timerawl.as_ptr())
+        };
 
+        let mono = Rp2040Monotonic::new(pac.TIMER);
 
         toggle_interrupt::spawn().ok();
-        background_task::spawn(0).ok();
-        count_clock::spawn().ok();
-
-        let timer = Time{seconds: 0, milliseconds: 0};
 
         (
             Shared {
                 in_pin,
-                timer,
-                rosc: rosc.initialize()
+                rosc: rosc.initialize(),
+                timer_regs,
+                sleep_time: 0,
+                interrupt_start_time: 0
             },
             Local {
-                led_pin: led,
+                number_of_tasks: 0,
+                last_timer_value: 0,
+                led_pin,
                 led_state: false
             },
             init::Monotonics(mono)
@@ -128,72 +132,113 @@ mod app {
     }
 
     
-    #[idle()]
-    fn idle_task(_ctx: idle_task::Context) -> ! {
+    #[idle(shared = [sleep_time, timer_regs])]
+    fn idle(mut ctx: idle::Context) -> ! {
+        defmt::info!("idle");
         loop {
+           critical_section::with(|_cs| {
+                let start_time = ctx.shared.timer_regs.lock(|timer_regs| {
+                    time_us_64(timer_regs.hi.0, timer_regs.lo.0)
+                });
+
+                rtic::export::wfi();
+
+                let end_time = ctx.shared.timer_regs.lock(|timer_regs| {
+                    time_us_64(timer_regs.hi.0, timer_regs.lo.0)
+                });
+
+                let sleep_time = end_time - start_time;
+
+                 ctx.shared.sleep_time.lock(|total_sleep_time| {
+                    *total_sleep_time = *total_sleep_time + sleep_time;
+                });
+            });
         }
     }
     
-    #[task(priority = 3, shared = [timer])]
-    fn count_clock(mut ctx: count_clock::Context) {
-       ctx.shared.timer.lock(|time| {
-            time.milliseconds += 1;
-            if time.milliseconds == 1000 {
-                time.seconds += 1;
-                time.milliseconds = 0;
-            }
-        });
-        let _ = count_clock::spawn_after(1.millis());
-    }
 
     // TODO: Add tasks
 // Toggle the led based on a local state
-    #[task(shared = [in_pin, rosc], priority = 2)]
-    fn toggle_interrupt(mut ctx: toggle_interrupt::Context) {
-        let in_pin = ctx.shared.in_pin;
-        let rosc = ctx.shared.rosc;
-        (in_pin, rosc).lock(|pin, rosc| {
+    #[task(shared = [in_pin, rosc, interrupt_start_time, timer_regs], local = [led_pin, led_state], priority = 2)]
+    fn toggle_interrupt(ctx: toggle_interrupt::Context) {
+        let mut in_pin = ctx.shared.in_pin;
+        (in_pin).lock(|pin| {
             let _ = pin.set_high();
         });
         // Re-spawn this task after 1000 milliseconds
-        let duration: u64 = 1000;
-        toggle_interrupt::spawn_after(duration.millis());
-    }
-    
-    #[task(binds = IO_IRQ_BANK0, priority = 2, shared = [in_pin], local = [led_state, led_pin])]
-    fn toggle_task(mut ctx: toggle_task::Context) {
-         ctx.shared.in_pin.lock(|pin| {
-            pin.set_low();
-        });
         if *ctx.local.led_state {
+            defmt::info!("led on");
             ctx.local.led_pin.set_high().unwrap();
             *ctx.local.led_state = false;
         } else {
+            defmt::info!("led off");
             ctx.local.led_pin.set_low().unwrap();
             *ctx.local.led_state = true;
         }
+        toggle_interrupt::spawn_after(CPU_PERIOD.secs()).ok();
+        let interrupt_start_time = ctx.shared.interrupt_start_time;
+        let timer_regs = ctx.shared.timer_regs;
+        (interrupt_start_time, timer_regs).lock(|start_time, timer_regs| {
+            *start_time = time_us_64(timer_regs.hi.0, timer_regs.lo.0);
+        });
+    }
+    
+    #[task(
+        binds = IO_IRQ_BANK0, 
+        priority = 2, 
+        local = [number_of_tasks, last_timer_value], 
+        shared = [in_pin, sleep_time, interrupt_start_time, timer_regs]
+    )]
+    fn toggle_task(mut ctx: toggle_task::Context) {
+         ctx.shared.in_pin.lock(|pin| {
+            pin.set_low().ok();
+        });
+        let interrupt_start_time = ctx.shared.interrupt_start_time;
+        let (start_time, end_time) = (interrupt_start_time, ctx.shared.timer_regs).lock(|start_time, timer_regs| {
+            let end_time = time_us_64(timer_regs.hi.0, timer_regs.lo.0);
+            (*start_time, end_time)
+        });
+
+        let interrupt_time = end_time - start_time;
+
+        let cpu_usage = (ctx.shared.sleep_time).lock(|sleep_time| {
+            let last_timer_value = *ctx.local.last_timer_value;
+            let current_timer_value = end_time;
+            let elapsed_time = current_timer_value - last_timer_value;
+
+            let cpu_usage = (elapsed_time as f64 - *sleep_time as f64) / (elapsed_time as f64);
+            *sleep_time = 0;
+            
+            *ctx.local.last_timer_value = end_time;
+            cpu_usage
+
+        });
+
+        defmt::info!("Interrupt time: {}us, background tasks: {}, CPU usage: {}%", 
+            interrupt_time, ctx.local.number_of_tasks, cpu_usage * 100 as f64);
+        *ctx.local.number_of_tasks = *ctx.local.number_of_tasks + 1;
+        background_task::spawn().ok();
 
     }
 
-    #[task(priority = 1, shared = [timer, rosc], capacity = 255)]
-    fn background_task(mut ctx: background_task::Context, task_id: u16) {
-        defmt::info!("New task: {}", task_id);
-        let start = ctx.shared.timer.lock(|time| {
-            return Time{seconds: time.seconds, milliseconds: time.milliseconds};
+    #[task(priority = 1, shared = [rosc, timer_regs], capacity = 255)]
+    fn background_task(mut ctx: background_task::Context) {
+        let start = ctx.shared.timer_regs.lock(|timer_regs| {
+            return time_us_64(timer_regs.hi.0, timer_regs.lo.0);
         });
         let (sleep_time, spawn_after) = ctx.shared.rosc.lock(|rosc| {
             return (get_random_byte(&rosc) % 10, get_random_byte(&rosc) % 10);
         });
+        defmt::info!("Sleep time: {}", sleep_time);
         loop {
-            let current_time = ctx.shared.timer.lock(|time| {
-                return Time{seconds: time.seconds, milliseconds: time.milliseconds};
+            let current_time = ctx.shared.timer_regs.lock(|timer_regs| {
+                return time_us_64(timer_regs.hi.0, timer_regs.lo.0);
             });
-            if current_time.seconds - start.seconds > sleep_time.into() {
+            if current_time - start > (sleep_time as u64) * 1_000_000 {
                 break;
             }
         }
-        let _ = background_task::spawn(task_id + 1);
-        let _ = background_task::spawn_after((spawn_after as u64).secs(), task_id);
+        let _ = background_task::spawn_after((spawn_after as u64).secs());
     }
 
 
--- pico/src/lib.rs
+++ pico/src/lib.rs
@@ -2,13 +2,12 @@
 #![no_std]
 
 use core::sync::atomic::{AtomicUsize, Ordering};
-use defmt_brtt as _; // global logger
+use defmt_rtt as _; // global logger
 
 use panic_probe as _;
 
 use rp2040_hal::rosc::{RingOscillator, Enabled};
 use rp_pico::hal as _; // memory layout
-
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
 // this prevents the panic message being printed *twice* when `defmt::panic` is invoked
 #[defmt::panic_handler]
@@ -24,6 +23,17 @@ defmt::timestamp!("{=usize}", {
     COUNT.store(n + 1, Ordering::Relaxed);
     n
 });
+
+pub const CPU_PERIOD: u64 = 3;
+
+pub struct TimerRegs {
+    pub hi: PointerWrapper,
+    pub lo: PointerWrapper
+}
+
+pub struct PointerWrapper(pub *mut u32);
+
+unsafe impl Send for PointerWrapper {}
 
 pub fn get_random_byte(rosc: &RingOscillator<Enabled>) -> u8 {
     let mut num: u8 = 0;
@@ -57,6 +67,21 @@ pub fn get_random_u64(rosc: &RingOscillator<Enabled>) -> u64 {
         array[i] = get_random_byte(&rosc)    
     }
     return u64::from_le_bytes(array)
+}
+
+pub fn time_us_64(timerawh: *mut u32, timerawl: *mut u32) -> u64 {
+    let mut awh_val = unsafe {core::ptr::read_volatile(timerawh)};
+    let mut awl_val: u32;
+    loop {
+        awl_val = unsafe {core::ptr::read_volatile(timerawl)};
+        let next_hi = unsafe {core::ptr::read_volatile(timerawh)};
+
+        if awh_val == next_hi {
+            break;
+        }
+        awh_val = next_hi;
+    }
+    return ((awh_val as u64) << 32 ) | (awl_val as u64);
 }
 
 /// Terminates the application and makes `probe-rs` exit with exit-code = 0
